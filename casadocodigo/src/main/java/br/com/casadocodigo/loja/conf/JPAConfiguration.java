c665afc274077892e905c043149b7945ea54694c
ruicosenza <ruicosenza@ruicosenza>
2016-08-24 23:15:56 -0300

    primeiro commit

---
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/conf/JPAConfiguration.java
@@ -0,0 +1,49 @@
+package br.com.casadocodigo.loja.conf;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+public class JPAConfiguration {
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+
+		factoryBean.setJpaVendorAdapter(vendorAdapter);
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		dataSource.setUrl("jdbc:hsqldb:mem:smsapiservicedb");
+		dataSource.setDriverClassName("org.hsqldb.jdbcDriver");
+
+		factoryBean.setDataSource(dataSource);
+
+		Properties props = new Properties();
+		props.setProperty("spring.jpa.hibernate.ddl-auto", "update");
+		props.setProperty("spring.jpa.hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
+		props.setProperty("hibernate.hbm2ddl.auto", "update");
+
+		factoryBean.setJpaProperties(props);
+
+		factoryBean.setPackagesToScan("br.com.casadocodigo.loja.models");
+
+		return factoryBean;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+		return new JpaTransactionManager(emf);
+	}
+}
