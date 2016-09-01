d024decf3e51df61a5d26c9e1e6e54e9bcc14a8a
ruicosenza <ruicosenza@ruicosenza>
2016-08-31 22:29:45 -0300

    segundo commit

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/models/Produto.java
@@ -1,5 +1,8 @@
 package br.com.casadocodigo.loja.models;
 
+import java.util.List;
+
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -14,6 +17,8 @@ public class Produto {
 	private String titulo;
 	private String descricao;
 	private int paginas;
+	@ElementCollection
+	private List<Preco> precos;
 
 	public int getId() {
 		return id;
@@ -47,9 +52,16 @@ public class Produto {
 		this.paginas = paginas;
 	}
 
-	@Override
-	public String toString() {
-		return "Produto [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	public List<Preco> getPrecos() {
+		return precos;
 	}
 
+	public void setPrecos(List<Preco> precos) {
+		this.precos = precos;
+	}
+
+	@Override
+	public String toString() {
+		return "Produto [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + "]";
+	}
 }
